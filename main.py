c975207858e84766349719a2e1ad8cfd91d5fe35
lexaphantom-dot <323722750+lexaphantom-dot@users.noreply.github.com>
2026-09-07 21:49:02 +0300

    delete venv pycache for repo

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/health")
+async def read_h():
+    return {"Hello": "World"}
+
 @app.get("/")
 async def read_root():
-    return {"Hello": "World"}
+    return {"pid": "or"}
